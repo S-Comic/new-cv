c5cac4fd279a2f2c4f0b9f17d5766b311123a279
Samuel Jule <32823294+S-Comic@users.noreply.github.com>
2022-12-14 15:58:47 +0000

    Re-visiting this project with a complete re-styling. Code is messy, next project will be re-creating this with cleaner code.

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
     </Head>
     
-    
+ 
     <main className="container">
 
         <section className={styles.heroBanner}>
