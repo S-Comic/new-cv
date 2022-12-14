29b40d9c3eab236b8db657f7346f23b2c5237af0
Samuel Jule <32823294+S-Comic@users.noreply.github.com>
2022-12-14 22:28:12 +0000

    bugfixing

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/home.module.css'
+import styles from '../styles/index.module.css'
 import Link from 'next/link'
 import { useState } from 'react'
 
