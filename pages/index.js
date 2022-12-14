1ded51857f95f150c2f93cf082e3c976d9e93221
Samuel Jule <32823294+S-Comic@users.noreply.github.com>
2022-12-14 22:22:31 +0000

    General cleaning, along with major style changes and responsive code for mobile/tablet

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/home.module.css'
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [isDarkMode, setDarkMode] = useState(true)
+
   return (
 
     
@@ -11,43 +14,41 @@ export default function Home() {
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
     </Head>
     
- 
-    <main className="container">
+    <div className={`${styles.allContentWrapper} ${isDarkMode ? styles.dark : ""}`}>
+    <div className={styles.darkToggleButton}>
+    <svg onClick={() => setDarkMode((lastState)=> !lastState)} viewBox="0 0 512 512">
+      <path d="M448 256c0-106-86-192-192-192V448c106 0 192-86 192-192zm64 0c0 141.4-114.6 256-256 256S0 397.4 0 256S114.6 0 256 0S512 114.6 512 256z"/>
+    </svg>
+    </div>
+
+    <main className={styles.sectionContainer}>
+
+        <section className={styles.videoContainer}>
+          <video autoPlay="autoplay" muted loop>
+            <source src="flowers.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </section>
+
+    
 
         <section className={styles.heroBanner}>
 
-
-          <div className={`${styles.heroBannerTextWrapper} lightBackground`}>
+          <div className={`${styles.heroBannerTextWrapper} ${isDarkMode ? styles.dark : ""}`}>
             <p>Hello, I am</p>
             <h1>SAMUEL JULE</h1>
             <p>Aspiring web-developer & designer in Jersey.
               Comfortable with pure CSS/HTML/JS & Wordpress,
-              learning frameworks such as React and Laravel.</p>
-            <div className="buttonContainer">
-              <Link href="/"><a className="solidButton">Blog</a></Link>
-              <Link href="/"><a className="borderedButton">Download CV</a></Link>
+              learning React. Lover of spreadsheets.</p>
+            <div className={styles.buttonContainer}>
+              <Link href="mailto:samjule@outlook.com"><a className={styles.solidButton}>Email</a></Link>
+              <Link href="/"><a className={styles.borderedButton}>Download CV</a></Link>
             </div>
           </div>
-          <img src={"./grouville.jpg"} />
-          <img src={"./me.jpg"} />
-          <img src={"./cliff.jpg"} />
+          
         </section>
 
 
-        <section className={`${styles.workSection} ${styles.lightBackground}`}>
-
-          <div className={styles.workSectionBanner}>
-
-          </div>
-
-        </section>
-
-        <div className='test'></div>
-
-
-
-
-
-      </main></>
+      </main></div></>
   )
 }
